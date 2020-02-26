e789924bf3dbd101effd8ac6bed35a366cd30183
Leslie Ariday Morales de la Garza <leslary2699@gmail.com>
2020-02-26 08:29:46 -0600

    Hasta aqui llegue

---
+++ BaseHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MvcPlantilla
+{
+    public class BaseHelper
+    {
+        public static int ejecutarsentencia (string sentencia,
+                                        CommandType tipo,
+            List<SqlParameter> parametros = null)
+        {
+
+           SqlConnection cnn = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+
+            try 
+	{	        
+		cnn.ConnectionString = "data source=SALA3-E37\SQLEXPRESS;user id=sa;password=Express;initial catalog=CUENTAS";
+                cnn.Open();
+                comando.Connection = cnn;
+                comando.CommandType = tipo;
+                comando.CommandText = "INSERT INTO Video VALUES (idVideo,titulo,repro,url)"
+	}
+	catch (Exception)
+	{
+		
+		throw;
+	}
+
+        
+    }
+}
